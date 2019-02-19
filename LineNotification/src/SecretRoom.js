d76c3c54a96783f53f9b6e16b7389335e0b06c90
KUROMAME724 <ajt7gjm24@gmail.com>
2019-02-20 01:10:43 +0900

    リハビリも兼ねてテストコード追加

--- LineNotification/src/SecretRoom.js
+++ LineNotification/src/SecretRoom.js
@@ -3,9 +3,12 @@ function secretRoomMain() {
     const targetDate = new Date("2015/11/01");
     const today = new Date();
 
+    var message = '';
     var termDay = Math.ceil((today - targetDate) / ONE_YEAR_MILLISECOND);
-
-    var message = "\n二人が付き合い初めて" + termDay + "日経過しました！"
-
-    sendHttpPost(message, secretRoomToken());
+    if (Number.isInteger(termDay/365)) {
+        message = \n二人が付き合い初めて" + termDay/365 + "年が経過しました！
+    }
+    message = "\n二人が付き合い初めて" + termDay + "日経過しました！"
+    Logger.log(message);
+    //sendHttpPost(message, secretRoomToken());
 }
